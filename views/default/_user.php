90ca07e6770795381a3ede3ce1037b5bdcbad470
krivobokruslan <hy_omfg@mail.ru>
2019-01-15 15:39:34 +0200

    fix user view

--- views/default/_user.php
+++ views/default/_user.php
@@ -8,7 +8,7 @@ $bundle = $this->getAssetManager()->getBundle(\krivobokruslan\fayechat\assets\Ch
 
 ?>
 
-<li id="user-<?php echo $user->getId(); ?>" data-user-id="<?php echo $user->getId(); ?>" class="user-row">
+<li id="user-<?php echo $user->getUserId(); ?>" data-user-id="<?php echo $user->getUserId(); ?>" class="user-row">
     <div class="user-panel">
         <div class="pull-left image">
             <img src="<?php echo $user->getAvatar() ? : $bundle->baseUrl . '/img/no-avatar.png'; ?>" width="50px" height="50px">
