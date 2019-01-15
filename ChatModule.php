dc29b00ddf46affcdd68ecddefdac7eb66583ded
krivobokruslan <hy_omfg@mail.ru>
2019-01-15 11:55:53 +0200

    remove bootstrap interface from module

--- ChatModule.php
+++ ChatModule.php
@@ -2,31 +2,15 @@
 
 namespace krivobokruslan\fayechat;
 
-use krivobokruslan\fayechat\interfaces\FayeServiceInterface;
-use krivobokruslan\fayechat\services\FayeService;
-use yii\base\BootstrapInterface;
 use yii\base\Module;
 
-class ChatModule extends Module implements BootstrapInterface
+class ChatModule extends Module
 {
     public $controllerNamespace = 'krivobokruslan\fayechat\controllers';
 
     public $fayeHost = 'http://localhost:8000';
 
     private $token = 'secret-faye-token';
-
-    public function bootstrap($app)
-    {
-        $container = \Yii::$container;
-
-        $container->setSingleton(FayeServiceInterface::class, function() use ($app) {
-            /**
-             * @var ChatModule $chatModule
-             */
-            $chatModule = $app->getModule('faye-chat');
-            return new FayeService($chatModule->getHost(), $chatModule->getToken());
-        });
-    }
 
     public function getHost()
     {
