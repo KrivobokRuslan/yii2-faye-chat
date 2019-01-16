b133ad2388e8cf01c40bb683314b18cf9b2e70bd
krivobokruslan <hy_omfg@mail.ru>
2019-01-16 14:21:27 +0200

    add dialogs column, fix styles

--- views/default/index.php
+++ views/default/index.php
@@ -6,22 +6,64 @@
 
 \krivobokruslan\fayechat\assets\ChatAssets::register($this);
 ?>
-<div class="row">
-    <div class="col-md-4">
-        <ul id="user-container">
-            <li id="user-template" data-user-id="" style="display: none;" class="user-row">
-                <h5 class="username"></h5>
-                <span class="user-status offline"></span>
-                <span class="text-status">Offline</span>
-            </li>
-            <?php foreach ($users as $user) {
-                echo $this->render('_user', [
-                    'user' => $user
-                ]);
-            } ?>
-        </ul>
+    <div class="row">
+        <div class="col-md-4">
+            <ul id="user-container" class="list-group">
+                <li id="user-template" data-user-id="" style="display: none;" class="user-row list-group-item">
+                    <div class="user-panel">
+                        <div class="pull-left image">
+                            <img src="<?php echo $bundle->baseUrl . '/img/no-avatar.png'; ?>" width="50px"
+                                 height="50px">
+                        </div>
+                        <div class="pull-left info">
+                            <p class="username"></p>
+                            <span class="user-status offline"></span>
+                            <span class="text-status">Offline</span>
+                        </div>
+                    </div>
+                </li>
+                <?php foreach ($users as $user) {
+                    echo $this->render('_user', [
+                        'user' => $user
+                    ]);
+                } ?>
+            </ul>
+        </div>
+        <div class="col-md-6" id="dialog-container">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="direct-chat-msg">
+                        <div class="direct-chat-info clearfix">
+                            <span class="direct-chat-name pull-left">Alexander Pierce</span>
+                            <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                        </div>
+                        <img class="direct-chat-img" src="<?php echo $bundle->baseUrl . '/img/no-avatar.png'; ?>">
+                        <div class="direct-chat-text">
+                            Is this template really for free? That's unbelievable!
+                        </div>
+                    </div>
+                    <div class="direct-chat-msg right">
+                        <div class="direct-chat-info clearfix">
+                            <span class="direct-chat-name pull-left">Alexander Pierce</span>
+                            <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                        </div>
+                        <img class="direct-chat-img" src="<?php echo $bundle->baseUrl . '/img/no-avatar.png'; ?>">
+                        <div class="direct-chat-text">
+                            Is this template really for free? That's unbelievable!
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Message...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Send</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 
 <?php
 $this->registerJsFile('http://localhost:8000/client.js');
