c22f027790508f95597e0ec164057928a1b89eb3
krivobokruslan <hy_omfg@mail.ru>
2019-01-17 23:48:02 +0200

    fix utf-8 encoding for mysql

--- migrations/m190113_141521_add_chat_module_tables.php
+++ migrations/m190113_141521_add_chat_module_tables.php
@@ -12,18 +12,22 @@ class m190113_141521_add_chat_module_tables extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%chat_users}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull(),
             'avatar' => $this->string()->null(),
             'status' => $this->smallInteger()->notNull()
-        ]);
+        ], $tableOptions);
 
         $this->createTable('{{%chat_dialogs}}', [
             'id' => $this->primaryKey(),
             'user_id_one' => $this->integer()->notNull(),
             'user_id_two' => $this->integer()->notNull()
-        ]);
+        ], $tableOptions);
 
         $this->addForeignKey('chat_dialog_user_id_two_fk', '{{%chat_dialogs}}', 'user_id_one', '{{%chat_users}}', 'id', 'CASCADE', 'RESTRICT');
         $this->addForeignKey('chat_dialog_user_id_one_fk', '{{%chat_dialogs}}', 'user_id_two', '{{%chat_users}}', 'id', 'CASCADE', 'RESTRICT');
@@ -33,7 +37,7 @@ class m190113_141521_add_chat_module_tables extends Migration
             'author_user_id' => $this->integer()->notNull(),
             'dialog_id' => $this->integer()->notNull(),
             'message' => $this->text()
-        ]);
+        ], $tableOptions);
 
         $this->addForeignKey('chat_dialog_id_fk', '{{%chat_dialog_messages}}', 'dialog_id', '{{%chat_dialogs}}', 'id', 'CASCADE', 'RESTRICT');
 
@@ -41,7 +45,7 @@ class m190113_141521_add_chat_module_tables extends Migration
             'id' => $this->primaryKey(),
             'dialog_message_id' => $this->integer()->notNull(),
             'file' => $this->string()->notNull()
-        ]);
+        ], $tableOptions);
 
         $this->addForeignKey('chat_dialog_message_id_fk', '{{%chat_dialog_files}}', 'dialog_message_id', '{{%chat_dialog_messages}}', 'id', 'CASCADE', 'RESTRICT');
     }
