9aebc1ec1227f5d8e55dbb81cada9e34893ee198
Ruslan Krivobok <krivobokruslan@gmail.com>
2019-07-28 21:33:17 +0300

    add room tabs

--- views/room/room.php
+++ views/room/room.php
@@ -6,19 +6,40 @@
  */
 ?>
 <div class="panel panel-default" id="room-content-<?php echo $room->id; ?>">
-    <div class="panel-body direct-chat-primary" id="message-container">
-        <?php
-        if (!empty($room->messages)) {
-            foreach ($room->messages as $message) {
-                echo $this->render('_message', [
-                    'message' => $message,
-                    'userId' => $userId
-                ]);
+    <ul class="nav nav-tabs" id="chatsTab" role="tablist">
+        <li class="nav-item active">
+            <a class="nav-link active" data-toggle="tab" role="tab" aria-controls="user-container" aria-selected="true" href="#message-container">Сообщения</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" role="tab" aria-controls="group-container" aria-selected="false" href="#room-member-tab">Участники</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="chatsTabContent">
+        <div role="tabpanel" aria-labelledby="room-message-tab" class="panel-body direct-chat-primary tab-pane fade active in" id="message-container">
+            <?php
+            if (!empty($room->messages)) {
+                foreach ($room->messages as $message) {
+                    echo $this->render('_message', [
+                        'message' => $message,
+                        'userId' => $userId
+                    ]);
+                }
+            } else {
+                echo 'There is no messages yet...';
             }
-        } else {
-            echo 'There is no messages yet...';
-        }
-        ?>
+            ?>
+        </div>
+        <div role="tabpanel" aria-labelledby="room-member-tab" class="panel-body direct-chat-primary tab-pane fade" id="room-member-tab">
+            <?php
+            if (!empty($room->members)) {
+                foreach ($room->members as $member) {
+                    echo $member->getChatUserId();
+                }
+            } else {
+                echo 'There is no messages yet...';
+            }
+            ?>
+        </div>
     </div>
     <div class="panel-footer">
         <div class="input-group">
