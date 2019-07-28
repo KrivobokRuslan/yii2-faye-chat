7922b4c7dfd5ef213613647cb9a98849b8d85ae0
Ruslan Krivobok <krivobokruslan@gmail.com>
2019-07-28 21:41:41 +0300

    fix tabs

--- views/room/room.php
+++ views/room/room.php
@@ -5,17 +5,18 @@
  * @var string $userId
  */
 ?>
-<div class="panel panel-default" id="room-content-<?php echo $room->id; ?>">
-    <ul class="nav nav-tabs" id="chatsTab" role="tablist">
-        <li class="nav-item active">
-            <a class="nav-link active" data-toggle="tab" role="tab" aria-controls="user-container" aria-selected="true" href="#message-container">Сообщения</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" role="tab" aria-controls="group-container" aria-selected="false" href="#room-member-tab">Участники</a>
-        </li>
-    </ul>
-    <div class="tab-content" id="chatsTabContent">
-        <div role="tabpanel" aria-labelledby="room-message-tab" class="panel-body direct-chat-primary tab-pane fade active in" id="message-container">
+
+<ul class="nav nav-tabs" id="chatsTab" role="tablist">
+    <li class="nav-item active">
+        <a class="nav-link active" data-toggle="tab" role="tab" aria-controls="user-container" aria-selected="true" href="#room-content-<?php echo $room->id; ?>">Сообщения</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" role="tab" aria-controls="group-container" aria-selected="false" href="#room-members-<?php echo $room->id; ?>">Участники</a>
+    </li>
+</ul>
+<div class="tab-content" id="chatsTabContent">
+    <div role="tabpanel" aria-labelledby="room-message-tab" class="panel panel-default tab-pane fade active in" id="room-content-<?php echo $room->id; ?>">
+        <div class="panel-body direct-chat-primary" id="message-container">
             <?php
             if (!empty($room->messages)) {
                 foreach ($room->messages as $message) {
@@ -29,7 +30,18 @@
             }
             ?>
         </div>
-        <div role="tabpanel" aria-labelledby="room-member-tab" class="panel-body direct-chat-primary tab-pane fade" id="room-member-tab">
+        <div class="panel-footer">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Message..." id="new-message">
+                <input type="hidden" id="room-id" value="<?php echo $room->id;?>">
+                <span class="input-group-btn">
+            <button class="btn btn-default" type="button" id="room-send-new-message">Send</button>
+        </span>
+            </div>
+        </div>
+    </div>
+    <div role="tabpanel" aria-labelledby="room-member-tab" class="panel panel-default tab-pane fade" id="room-members-<?php echo $room->id; ?>">
+        <div class="panel-body direct-chat-primary">
             <?php
             if (!empty($room->members)) {
                 foreach ($room->members as $member) {
@@ -39,15 +51,6 @@
                 echo 'There is no messages yet...';
             }
             ?>
-        </div>
-    </div>
-    <div class="panel-footer">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Message..." id="new-message">
-            <input type="hidden" id="room-id" value="<?php echo $room->id;?>">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button" id="room-send-new-message">Send</button>
-            </span>
         </div>
     </div>
 </div>
