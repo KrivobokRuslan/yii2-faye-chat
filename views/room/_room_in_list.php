104a4f9d5abc9748f20582da029043f370b7a66c
Ruslan Krivobok <krivobokruslan@gmail.com>
2019-07-30 14:14:30 +0300

    fix room leave\delete

--- views/room/_room_in_list.php
+++ views/room/_room_in_list.php
@@ -18,12 +18,12 @@ $bundle = $this->getAssetManager()->getBundle(\krivobokruslan\fayechat\assets\Ch
             <span class="text-status">Участников: <span class="members-count"><?php echo $room->getCountMembers(); ?></span></span>
         </div>
         <?php if($room->isOwner(Yii::$app->user->id)): ?>
-            <div class="pull-right room-leave" data-room-id="<?php echo $room->id;?>" style="font-size: 25px; margin-top: 10px; margin-right: 5px;">
-                <span class="glyphicon glyphicon-log-out"></span>
-            </div>
-        <?php else : ?>
             <div class="pull-right room-delete" data-room-id="<?php echo $room->id;?>" style="font-size: 25px; margin-top: 10px; margin-right: 5px;">
                 <span class="glyphicon glyphicon-trash"></span>
+            </div>
+        <?php else : ?>
+            <div class="pull-right room-leave" data-room-id="<?php echo $room->id;?>" style="font-size: 25px; margin-top: 10px; margin-right: 5px;">
+                <span class="glyphicon glyphicon-log-out"></span>
             </div>
         <?php endif; ?>
     </div>
