8f7df5c942bc7554acbef418449a487ea17d2e4f
Ruslan Krivobok <krivobokruslan@gmail.com>
2019-07-30 18:46:55 +0300

    fix dp

--- converted/User.php
+++ converted/User.php
@@ -3,15 +3,17 @@
 namespace krivobokruslan\fayechat\converted;
 
 use \krivobokruslan\fayechat\entities\User as pUser;
+use yii\base\Model;
 
-class User
+class User extends Model
 {
     public $id;
     public $username;
     public $avatar;
 
-    public function __construct(pUser $user)
+    public function __construct(pUser $user, array $config = [])
     {
+        parent::__construct($config);
         $this->id = $user->getChatUserId();
         $this->username = $user->getChatUsername();
         $this->avatar = $user->getChatAvatar();
